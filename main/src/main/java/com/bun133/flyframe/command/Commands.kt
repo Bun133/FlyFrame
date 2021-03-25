48fe18d670841a1bbe5559a59fea882e844ebefe
Bun133 <bun133.work@gmail.com>
2021-03-25 17:08:19 +0900

    Init

---
+++ main/src/main/java/com/bun133/flyframe/command/Commands.kt
@@ -0,0 +1,73 @@
+package com.bun133.flyframe.command
+
+import com.bun133.flyframe.FlyFrame
+import com.bun133.flyframe.flylib.SmartTabCompleter
+import com.bun133.flyframe.flylib.TabChain
+import com.bun133.flyframe.flylib.TabObject
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class Commands(val plugin: FlyFrame) {
+    fun onEnable() {
+        plugin.getCommand("fm")!!.setExecutor(FMCommand(plugin))
+        plugin.getCommand("fm")!!.tabCompleter = FMCommand.gen()
+    }
+}
+
+class FMCommand(val plugin: FlyFrame) : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender is Player) {
+            if (sender.isOp) {
+                return run(sender, command, label, args)
+            } else {
+                sender.sendMessage("You don't have enough permission!")
+                return false
+            }
+        } else return run(sender, command, label, args)
+    }
+
+    fun run(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        when (args.size) {
+            1 -> {
+                when (args[0]) {
+                    "list" -> {
+                        val desc =
+                            plugin.modules.map { module -> Pair(Pair(module.moduleName, module.version),module.authorName) }.toTypedArray()
+                        desc.forEach {
+                            sender.sendMessage("Name:${it.first.first} Version:${it.first.second} Author:${it.second}")
+                        }
+                        sender.sendMessage("${desc.size} Modules are loaded!")
+                        return true
+                    }
+                    "help" -> {
+                        var authorString = ""
+                        plugin.description.authors.forEach { authorString += "$it " }
+                        sender.sendMessage("FlyFrame Version:${plugin.description.version} Authors:${authorString}")
+                        return true
+                    }
+                    else -> return false
+                }
+            }
+            else -> return false
+        }
+    }
+
+    companion object{
+        fun gen(): SmartTabCompleter {
+            return SmartTabCompleter(
+                TabChain(
+                    TabObject(
+                        "list"
+                    )
+                ),
+                TabChain(
+                    TabObject(
+                        "help"
+                    )
+                )
+            )
+        }
+    }
+}
---
+++ main/src/main/java/com/bun133/flyframe/util/MessageSendable.kt
@@ -0,0 +1,28 @@
+@file:Suppress("SpellCheckingInspection")
+
+package com.bun133.flyframe.util
+
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+
+abstract class MessageSendable {
+    abstract fun send(mes:String)
+}
+
+class CommandSendable(private val se:CommandSender): MessageSendable() {
+    override fun send(mes: String) {
+        se.sendMessage(mes)
+    }
+}
+
+class SeverSendable():MessageSendable() {
+    override fun send(mes: String) {
+        println(mes)
+    }
+}
+
+class BroadcastSendable():MessageSendable(){
+    override fun send(mes: String) {
+        Bukkit.broadcastMessage(mes)
+    }
+}
